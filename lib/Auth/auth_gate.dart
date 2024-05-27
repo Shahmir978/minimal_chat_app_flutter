1f4ca2a3ce4d1f9c8f3f139cfbe7683d70041bd9
Shahmir978 <shahmirk978@gmail.com>
2024-05-27 15:35:41 +0500

    authgate done

---
+++ lib/Auth/auth_gate.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:minimal_chat_app_flutter/Auth/Login_or_Register.dart';
+import 'package:minimal_chat_app_flutter/Pages/Home_Page.dart';
+
+class AuthGate extends StatefulWidget {
+  const AuthGate({super.key});
+
+  @override
+  State<AuthGate> createState() => _AuthGateState();
+}
+
+class _AuthGateState extends State<AuthGate> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(stream: FirebaseAuth.instance.authStateChanges(), builder: (context,snapshot){
+
+        // user is logged in
+        if(snapshot.hasData){
+          return  HomePage();
+        }
+        // user is NOT logged in
+        else {
+          return LoginOrRegister();
+        }
+
+      }),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:minimal_chat_app_flutter/Auth/Login_or_Register.dart';
+import 'package:minimal_chat_app_flutter/Auth/auth_gate.dart';
 
 import 'package:minimal_chat_app_flutter/Themes/Light_Mode.dart';
 import 'package:minimal_chat_app_flutter/firebase_options.dart';
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: lightMode,
-      home: LoginOrRegister(),
+      home: const AuthGate(),
     );
   }
 }
