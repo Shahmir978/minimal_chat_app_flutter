1de0ec953f36473205beb6a85ce8a8929620e015
Shahmir978 <shahmirk978@gmail.com>
2024-05-27 16:58:11 +0500

    drawer work done

---
+++ lib/Components/makeDrawer.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:minimal_chat_app_flutter/Auth/auth_service.dart';
+import 'package:minimal_chat_app_flutter/Pages/Settings_Page.dart';
+
+class makeDrawer extends StatelessWidget {
+  const makeDrawer({super.key});
+void logout ()async{
+  // get auth service
+  final _auth = AuthService();
+  _auth.signOut();
+}
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              DrawerHeader(
+                child: Center(
+                  child: Icon(
+                    Icons.message,
+                    size: 64,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+              ),
+
+              // home list tile
+              Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child: ListTile(
+                  leading: Icon(Icons.home),
+                  title: Text("H O M E"),
+                  onTap: () {
+                    //pop the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+
+              // settings list tile
+              Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child: ListTile(
+                  leading: Icon(Icons.settings),
+                  title: Text("S E T T I N G S"),
+                  onTap: () {
+                    //pop the drawer
+                    Navigator.pop(context);
+                    //navigate to settingspage
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => SettingsPage()));
+                  },
+                ),
+              ),
+            ],
+          ),
+
+          // logout list tile
+          Padding(
+            padding: const EdgeInsets.only(left: 25, bottom: 25),
+            child: ListTile(
+              leading: Icon(Icons.logout),
+              title: Text("L O G O U T"),
+              onTap: logout,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/Pages/Home_Page.dart
+++ lib/Pages/Home_Page.dart
@@ -1,24 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:minimal_chat_app_flutter/Auth/auth_service.dart';
+import 'package:minimal_chat_app_flutter/Components/makeDrawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-void logout ()async{
-  // get auth service
-  final _auth = AuthService();
-  _auth.signOut();
-}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('Home Page'), actions: [
-        IconButton(
-          onPressed:logout,
-          icon: Icon(Icons.logout_outlined),
-        ),
-      ]),
+        title: Text('Home Page'),
+      ),
+      drawer: makeDrawer(),
     );
   }
 }
